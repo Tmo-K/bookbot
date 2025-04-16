e42ab1d42ca644bdfc4cbb851759a06c78f87eff
Tmo-K <timo.kwoll@gmail.com>
2025-04-16 23:02:08 +0200

    Move count_words function into new file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import count_words
+
 def main():
     path_to_file = "books/frankenstein.txt"
     file = read_file(path_to_file)
@@ -10,11 +12,6 @@ def read_file(path_to_file):
     with open(path_to_file) as f:
         file_content = f.read()
     return file_content
-
-def count_words(file_content):
-    words = file_content.split()
-    word_count = len(words)
-    return word_count
 
 def count_characters(file_conent):
     alphabet = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0, }
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(file_content):
+    words = file_content.split()
+    word_count = len(words)
+    return word_count
